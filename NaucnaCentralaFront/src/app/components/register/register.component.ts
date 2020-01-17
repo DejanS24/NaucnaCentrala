fbb59a29d630174a2288f7f2b2c896dc9c6a1709
DejanS24 <dejans1224@gmail.com>
2020-01-17 23:56:04 +0100

    Added front correctly

---
+++ NaucnaCentralaFront/src/app/components/register/register.component.ts
@@ -0,0 +1,79 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { ProcessService } from 'src/app/services/process.service';
+
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+    private formFieldsDto = null;
+    private formFields = [];
+    private processInstanceId = '';
+    private enumValues = [];
+    private tasks = [];
+    private nextTask = '';
+    taskId = '';
+
+
+  constructor(private userService: UserService, private processService: ProcessService, private router: Router) {
+
+    const x = processService.startProcess();
+
+    x.subscribe(
+      res => {
+        // console.log(res);
+        // this.categories = res;
+        this.taskId = res.taskId;
+        // console.log('dobijeno je ' + res);
+        // console.log('id taska je: ' + this.taskId);
+        this.formFieldsDto = res;
+        this.formFields = res.formFields;
+        this.processInstanceId = res.processInstanceId;
+
+      },
+      err => {
+        console.log('Error occured');
+      }
+    );
+   }
+
+    getId() {
+    return this.processInstanceId;
+
+   }
+
+
+  ngOnInit() {
+  }
+
+  onSubmit(value, form) {
+    const o = new Array();
+    for (const property in value) {
+      // console.log(property);
+      // console.log(value[property]);
+      o.push({fieldId : property, fieldValue : value[property]});
+    }
+
+    // console.log(o);
+    const x = this.userService.registerUserDammy(o, this.formFieldsDto.taskId);
+
+    x.subscribe(
+      res => {
+        // console.log(res);
+
+        alert('You registered successfully!');
+        this.router.navigate(['/task',  this.processInstanceId]);
+      },
+      err => {
+        console.log('Error occured');
+        alert('Value of field is empty or not valid!');
+
+      }
+    );
+  }
+}
